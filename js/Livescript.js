c3cb712e94bf0c448527445cf08d13ff44a97481
PanoramaView <zhou-1997@hotmail.it>
2021-04-19 16:49:11 +0200

    Ok il calcolo funziona

--- js/Livescript.js
+++ js/Livescript.js
@@ -1,4 +1,6 @@
 window.addEventListener("load", function () {
+    var totale = 50;
+    console.log("Base price: " + totale);
     script();
 })
 function script() {
@@ -6,29 +8,32 @@ function script() {
     var myBtn = document.getElementById("button");
 
     myBtn.addEventListener("click", function (event) {
-        //prendo dati
-        var FieldBurger = document.getElementById("burgername");
-        var FieldCheese = document.getElementById("cheese-add");
-        var FieldEgg = document.getElementById("egg-add");
-        var FieldLettuce = document.getElementById("lettuce-add");
-        var FieldMustard = document.getElementById("mustard-add");
-        var FieldKetchup = document.getElementById("ketchup-add");
-
         event.preventDefault(); //impedisce il refresh del sito
+        //prendo dati
+        // var FieldBurger = document.getElementById("burgername");
+        // var FieldCheese = document.getElementById("cheese-add");
+        // var FieldEgg = document.getElementById("egg-add");
+        // var FieldLettuce = document.getElementById("lettuce-add");
+        // var FieldMustard = document.getElementById("mustard-add");
+        // var FieldKetchup = document.getElementById("ketchup-add");
+        
         var arrayIngredient = new Array();
-        var arrayIngredientHTML = document.querySelectorAll("[value^='ingredient']")
-
+        var arrayIngredientHTML = document.querySelectorAll("[name^='ingredient']")
+        //pusho in un array tutti i value degli ingredienti checkati
         for (var i = 0; i < arrayIngredientHTML.length; i++) {
             var x = arrayIngredientHTML[i];
             if(x.checked){
                 arrayIngredient.push(x.value);
             }
-            
+        }
+        //sommo
+        for (var i = 0; i < arrayIngredient.length; i++) {
+            totale += parseInt(arrayIngredient[i]);
         }
 
+        //print output
+        console.log("Totale: "+ totale);
+        var PriceHTML = document.getElementById("price");
+        PriceHTML.innerHTML= totale +" €";
     })
-}
-function addIngredient(tot, ingPrice) {
-    tot += ingPrice;
-    return tot;
 }
