838fd4268526319d9683159d84b0d7f73f7a9768
PanoramaView <zhou-1997@hotmail.it>
2021-04-19 17:30:24 +0200

    Codice funziona

--- js/Livescript.js
+++ js/Livescript.js
@@ -1,39 +1,51 @@
 window.addEventListener("load", function () {
     var totale = 50;
     console.log("Base price: " + totale);
+    var couponList = ["FLORIAN2021", "BOOLEAN2021", "MARCO420SIXTYNINE"];
+    console.log("Coupon list: \n" + couponList.join("\n"));
     script();
-})
+});
+
 function script() {
-    
+
     var myBtn = document.getElementById("button");
 
     myBtn.addEventListener("click", function (event) {
         event.preventDefault(); //impedisce il refresh del sito
         //prendo dati
-        // var FieldBurger = document.getElementById("burgername");
-        // var FieldCheese = document.getElementById("cheese-add");
-        // var FieldEgg = document.getElementById("egg-add");
-        // var FieldLettuce = document.getElementById("lettuce-add");
-        // var FieldMustard = document.getElementById("mustard-add");
-        // var FieldKetchup = document.getElementById("ketchup-add");
-        
+
         var arrayIngredient = new Array();
         var arrayIngredientHTML = document.querySelectorAll("[name^='ingredient']")
         //pusho in un array tutti i value degli ingredienti checkati
         for (var i = 0; i < arrayIngredientHTML.length; i++) {
             var x = arrayIngredientHTML[i];
-            if(x.checked){
+            if (x.checked) {
                 arrayIngredient.push(x.value);
             }
         }
-        //sommo
+        //sommo 
+        var totale = 50;
         for (var i = 0; i < arrayIngredient.length; i++) {
             totale += parseInt(arrayIngredient[i]);
         }
 
-        //print output
-        console.log("Totale: "+ totale);
-        var PriceHTML = document.getElementById("price");
-        PriceHTML.innerHTML= totale +" €";
-    })
+        //coupon
+        var checkcoupon = false;
+        var couponList = ["FLORIAN2021", "BOOLEAN2021", "MARCO420SIXTYNINE"];
+        var CouponHTML = document.getElementById("coupon");
+        var Couponcode = CouponHTML.value;
+        for (var i = 0; i < couponList.length; i++) {
+            if (Couponcode === couponList[i]) {
+                checkcoupon = true;
+            }
+        }
+        if (checkcoupon) {
+            totale = totale * 0.8;
+            console.log("20% discount has been applied.")
+        }
+        console.log("Totale: " + totale);
+        var codeHTML = document.getElementById("price");
+        codeHTML.innerHTML = totale + " €";
+    });
+
 }
